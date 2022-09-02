3b7128194d86e96b8b8a51de092546d8b2f08e84
Yevhen Kornilov <zhenyakornilov98@gmail.com>
2022-09-02 22:44:06 +0300

    fetch and render users on page testing

--- js/index.js
+++ js/index.js
@@ -1,16 +1,38 @@
 const RANDOM_USERS_URL =
-  "https://randomuser.me/api/1.4/?results=10&nat=ua,us,de,dk,fr,gb&inc=gender,name,age,registered,phone,cell,picture,nat,email";
+  "https://randomuser.me/api/1.4/?results=10&nat=ua,us,de,dk,fr,gb&inc=gender,name,dob,registered,phone,cell,picture,nat,email";
 
+const allUsers = document.getElementById("allUsers");
 
 async function fetchUsers(url) {
   let res = await fetch(url);
   usersData = await res.json();
-  return usersData.results
+  return usersData.results;
 }
 
 async function renderUsers() {
-    let users = await fetchUsers(RANDOM_USERS_URL)
-    console.log(users);
+  let users = await fetchUsers(RANDOM_USERS_URL);
+  console.log(users);
+  let usersHTML = users
+    .map(
+      ({ gender, name, dob, registered, phone, cell, picture, nat, email }) => {
+        return `
+        <li class="user-card">
+          <span>${gender}</span>
+          <span>${name.first}</span>
+          <span>${dob.age}</span>
+          <span>${registered.date}</span>
+          <span>${phone}</span>
+          <span>${cell}</span>
+          <img src="${picture.imageUrl}">
+          <span>${nat}</span>
+          <span>${email}</span>
+        </li>
+          `;
+      }
+    )
+    .join("");
+
+  allUsers.innerHTML = usersHTML;
 }
 
 renderUsers();

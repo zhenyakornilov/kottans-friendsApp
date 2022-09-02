c39bd13227aa2fe76e44737531b2b01d6173095f
Yevhen Kornilov <zhenyakornilov98@gmail.com>
2022-09-02 22:11:14 +0300

    Learn and test Fetch API

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-console.log('hello');
+const RANDOM_USERS_URL =
+  "https://randomuser.me/api/1.4/?results=10&nat=ua,us,de,dk,fr,gb&inc=gender,name,age,registered,phone,cell,picture,nat,email";
+
+async function fetchText(url) {
+  let response = await fetch(url);
+  let data = await response.json();
+  console.log(data.results);
+}
+
+fetchText(RANDOM_USERS_URL)

be719b0bf7350750d78ab657b2df71ce2fe6bfac
Yevhen Kornilov <zhenyakornilov98@gmail.com>
2022-09-02 22:26:32 +0300

    Fetch users

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
 const RANDOM_USERS_URL =
   "https://randomuser.me/api/1.4/?results=10&nat=ua,us,de,dk,fr,gb&inc=gender,name,age,registered,phone,cell,picture,nat,email";
 
-async function fetchText(url) {
-  let response = await fetch(url);
-  let data = await response.json();
-  console.log(data.results);
+
+async function fetchUsers(url) {
+  let res = await fetch(url);
+  usersData = await res.json();
+  return usersData.results
 }
 
-fetchText(RANDOM_USERS_URL)
+async function renderUsers() {
+    let users = await fetchUsers(RANDOM_USERS_URL)
+    console.log(users);
+}
+
+renderUsers();

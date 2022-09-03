6fafb423507d139ef052f4873bfd4f04ff11b54a
Yevhen Kornilov <zhenyakornilov98@gmail.com>
2022-09-03 21:14:28 +0300

    Add some styles and layout

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const RANDOM_USERS_URL =
-  "https://randomuser.me/api/1.4/?results=10&nat=ua,us,de,dk,fr,gb&inc=gender,name,dob,registered,phone,cell,picture,nat,email";
+  "https://randomuser.me/api/1.4/?results=20&nat=ua,us,de,dk,fr,gb&inc=gender,name,dob,registered,phone,picture,nat,email";
 
 const allUsers = document.getElementById("allUsers");
 
@@ -11,25 +11,24 @@ async function fetchUsers(url) {
 
 async function renderUsers() {
   let users = await fetchUsers(RANDOM_USERS_URL);
-  console.log(users);
   let usersHTML = users
-    .map(
-      ({ gender, name, dob, registered, phone, cell, picture, nat, email }) => {
-        return `
-        <li class="user-card">
-          <span>${gender}</span>
-          <span>${name.first}</span>
-          <span>${dob.age}</span>
-          <span>${registered.date}</span>
-          <span>${phone}</span>
-          <span>${cell}</span>
-          <img src="${picture.imageUrl}">
-          <span>${nat}</span>
-          <span>${email}</span>
+    .map(({ gender, name, dob, phone, picture, email }) => {
+      let userFullName = `${name.first} ${name.last}`;
+      let imageSrc = picture.large;
+      return `
+        <li class="user-profile-card">
+          <img src="${imageSrc}" class="profile-photo" alt="photo of ${userFullName}">
+          <div class="user-main-info">
+            <span class="user-info">${userFullName}</span>
+            <span class="user-info">${gender}, ${dob.age} y.o.</span>
+          </div>
+          <div class="user-contacts">
+            <span class="user-contact">Phone: ${phone}</span>
+            <span class="user-contact">Email: ${email}</span>
+          </div>
         </li>
           `;
-      }
-    )
+    })
     .join("");
 
   allUsers.innerHTML = usersHTML;

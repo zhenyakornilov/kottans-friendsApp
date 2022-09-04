17e84c8e66fda6cbb80bbeea6e685c7816740c29
Yevhen Kornilov <zhenyakornilov98@gmail.com>
2022-09-04 15:44:25 +0300

    Add form reset button

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@ const RANDOM_USERS_URL =
   "https://randomuser.me/api/1.4/?results=20&nat=ua,us,de,dk,fr,gb&inc=gender,name,dob,registered,phone,picture,nat,email";
 
 const allUsers = document.getElementById("allUsers");
+const searchByName = document.getElementById("searchByName");
+const filterForm = document.getElementById("filterForm");
+const resetFilter = document.getElementById("resetFilter");
 
 async function fetchUsers(url) {
   let res = await fetch(url);
@@ -37,3 +40,13 @@ async function renderUsers() {
 }
 
 renderUsers();
+
+searchByName.addEventListener("keyup", function () {
+  let counter = 0;
+  console.log(counter);
+  counter++;
+});
+
+resetFilter.addEventListener("click", () => {
+  filterForm.reset();
+});
